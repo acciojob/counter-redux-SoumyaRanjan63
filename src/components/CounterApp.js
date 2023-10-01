1c8b65c6e451519f570bce7c33c2a225155e617d
SoumyaRanjan63 <sranjan6370@gmail.com>
2023-10-01 16:33:52 +0530

    trd commit

--- src/components/CounterApp.js
+++ src/components/CounterApp.js
@@ -7,7 +7,7 @@ const CounterApp = () => {
     const dispatch=useDispatch();
     return (
         <div>
-            <h1>Count : {count}</h1>
+            <h1>{count}</h1>
             <button onClick={()=>dispatch(increment())}>Increment</button>
             <button onClick={()=>dispatch(decrement())}>Decrease</button>
 
